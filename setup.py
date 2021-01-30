1122a8e664fb5d53c383bf1df50d180d896ea05e
Michael Anckaert <michael.anckaert@sinax.be>
2021-01-30 21:35:49 +0100

    Update build steps and fix some issues

--- setup.py
+++ setup.py
@@ -9,13 +9,21 @@ setuptools.setup(
     author="Michael Anckaert",
     author_email="michael.anckaert@sinax.be",
     description="Galax is a static site generator targetting both HTML and Gemini",
+    keywords='static site generator gemini html',
     long_description=long_description,
-    url="https://github.com/MichaelAnckaert/galax",
+    long_description_content_type='text/markdown',
     packages=setuptools.find_packages(),
+    url="https://github.com/MichaelAnckaert/galax",
+    scripts=['bin/galax'],
+    python_requires='>=3.6',
+    install_requires=[
+        "Markdown==3.3.3",
+        "Jinja2==2.11.2"
+    ],
     classifiers=[
+        "Development Status :: 4 - Beta",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
 )
